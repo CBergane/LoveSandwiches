695a23867b49cd23df815abe78ced528437d2cc0
Christian Bergane <107763472+CBergane@users.noreply.github.com>
2022-09-22 10:35:16 +0200

    added surplus data to workseet

--- run.py
+++ run.py
@@ -59,6 +59,15 @@ def update_sales_worksheet(data):
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
+
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet.
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully.\n")
     
     
 def calulate_sureplus_data(sales_row):
@@ -78,6 +87,7 @@ def calulate_sureplus_data(sales_row):
         surplus_data.append(surplus)
     
     return surplus_data
+
     
 
 def main():
@@ -88,7 +98,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calulate_sureplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
     
 
 print("Welcome to Love Sandwiches Data Automation")
