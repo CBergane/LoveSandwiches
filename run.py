306ba2796a0f0502526d29fc23e897ec44f6484c
Christian Bergane <107763472+CBergane@users.noreply.github.com>
2022-09-22 12:09:33 +0200

    final commit

--- run.py
+++ run.py
@@ -12,26 +12,30 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
-    Get sales figures from the user
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
-        print("Please enter sales figures from the last market.")
+        print("Please enter sales data from the last market.")
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here:\n")
 
         sales_data = data_str.split(",")
-        
+
         if validate_data(sales_data):
-            print("Data is valid")
+            print("Data is valid!")
             break
-    
+
     return sales_data
-    
-    
+
+
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
@@ -49,7 +53,7 @@ def validate_data(values):
         return False
 
     return True
-    
+
 
 def update_worksheet(data, worksheet):
     """
@@ -59,40 +63,43 @@ def update_worksheet(data, worksheet):
     print(f"Updating {worksheet} worksheet...\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
-    print(f"{worksheet} worksheet updated successfully.\n")
-    
-    
+    print(f"{worksheet} worksheet updated successfully\n")
+
+
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
     The surplus is defined as the sales figure subtracted from the stock:
-    - Positive surplus indicates waste.
-    - Negative surplus indicates extra made when stock ran out.
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
     """
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
-    stock_rwo = stock[-1]
+    stock_row = stock[-1]
     
     surplus_data = []
-    for stock, sales in zip(stock_rwo, sales_row):
+    for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
-    
+
     return surplus_data
 
-def get_last_5_entry_sales():
+
+def get_last_5_entries_sales():
     """
-    Collect columns of data from sales worksheet, collecting the last 5 entries for each sandwiches and return the data as a list of lists.
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
     """
     sales = SHEET.worksheet("sales")
-    # column = sales.col_values(3)
-    # print(column)
-    
+
     columns = []
     for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
+
     return columns
+
 
 def calculate_stock_data(data):
     """
@@ -100,14 +107,15 @@ def calculate_stock_data(data):
     """
     print("Calculating stock data...\n")
     new_stock_data = []
-    
+
     for column in data:
         int_column = [int(num) for num in column]
         average = sum(int_column) / len(int_column)
         stock_num = average * 1.1
         new_stock_data.append(round(stock_num))
-        
+
     return new_stock_data
+
 
 def main():
     """
@@ -118,9 +126,10 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
-    sales_columns = get_last_5_entry_sales()
+    sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
